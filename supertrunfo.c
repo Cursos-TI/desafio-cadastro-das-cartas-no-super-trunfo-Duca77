a7661b1c898f57c39b2e0620a044c9cd0ca2e6af
Duca77 <ducajulio66@yahoo.com.br>
2025-05-05 13:32:56 +0000

    Carta 1

--- supertrunfo.c
+++ supertrunfo.c
@@ -3,4 +3,34 @@ int main(){
 
     printf(" Desafio supertrunfo\n");
     printf(" Novo commit\n");
+
+    char estado[20];
+    char codigo[20];
+    char cidade[20];
+    int populacao;
+    float area;
+    float pib;
+    int pontos_turisticos;
+
+    printf("Digite o estado: ");
+    scanf("%s", estado);
+// O estado é uma string, então usamos %s para ler a string 
+    printf("Digite o codigo: ");
+    scanf("%s", codigo);
+// O codigo é uma string, então usamos letra e número, então usamos %s para ler a string
+    printf("Digite a cidade: ");
+    scanf("%s", cidade);
+// O nome da cidade é uma string, então usamos %s para ler a string também 
+    printf("Digite a populacao: ");
+    scanf("%d", &populacao);
+// A populacao é um inteiro, então usamos %d para ler o inteiro 
+    printf("Digite a area: ");
+    scanf("%f", &area);
+// A area é um float, então usamos %f para ler o float 
+    printf("Digite o pib: ");
+    scanf("%f", &pib);
+// O pib é um float, então usamos %f para ler o float também 
+    printf("Digite o numero de pontos turisticos: ");
+    scanf("%d", &pontos_turisticos); 
+// O numero de pontos turisticos é um inteiro, então usamos %d para ler o inteiro 
 }
